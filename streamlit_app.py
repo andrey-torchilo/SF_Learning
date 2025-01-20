018ac7125fe3361774933a4790d5aa51ca4fb748
andrey-torchilo <152385429+andrey-torchilo@users.noreply.github.com>
2025-01-20 20:05:59 +0100

    Update streamlit_app.py
    rearrenged blocks

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,12 @@ if ingredients_list:
     st.write(ingredients_list)
     st.text(ingredients_list)
 
-    ingredients_string = ' '.join(ingredients_list)
+    #ingredients_string = ' '.join(ingredients_list)
     #st.text(ingredients_string)
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
@@ -43,6 +47,4 @@ if ingredients_list:
          session.sql(my_insert_stmt).collect()
          st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
